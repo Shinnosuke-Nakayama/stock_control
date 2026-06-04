bd6ff22438c1d4a072e1002443e0d7e044270386
shinnosuke <nakayama.shinnosuke@outlook.jp>
2026-06-05 00:02:50 +0900

    TRY

--- knexfile.js
+++ knexfile.js
@@ -4,22 +4,6 @@ require("dotenv").config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      user: "user",
-      database: "stock_control",
-      host: "localhost",
-    },
-    migrations: {
-      directory: "./knex_migrations",
-    },
-    seeds: {
-      directory: "./knex_seeds",
-    },
-  },
-
-  production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
@@ -28,5 +12,4 @@ module.exports = {
     seeds: {
       directory: "./knex_seeds",
     },
-  },
 };
